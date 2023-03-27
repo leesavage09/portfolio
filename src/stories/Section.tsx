e3d6b747a7f15180f189d994ebacf1861eec4c93
Lee <leesavage09@gmail.com>
2023-03-27 16:19:47 +0300

    style: section styles are improved

--- src/stories/Section.tsx
+++ src/stories/Section.tsx
@@ -6,7 +6,22 @@ interface SectionProps {
 
 export const Section: React.FC<SectionProps> = ({ children }) => {
   return (
-    <section className="bg-blue dark:bg-dark-blue text-white flex flex-col justify-center items-center p-9 bg-pattern bg-cover">
+    <section
+      className="
+    bg-blue-200 
+    text-dark-blue-800 
+
+    dark:bg-dark-blue 
+    dark:text-blue-100 
+    
+    flex flex-col justify-center items-center 
+    
+    p-9
+    h-screen
+    
+    bg-pattern bg-cover
+    "
+    >
       {children}
     </section>
   );
