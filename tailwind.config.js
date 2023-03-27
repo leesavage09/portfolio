a985504653a7be487384183515ce7db655d4dac9
Lee <leesavage09@gmail.com>
2023-03-27 14:58:35 +0300

    feat: added tailwind design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,58 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    colors: {
+      black: '#000000',
+      white: '#ffffff',
+      primary: {
+        100: '#FEE7E7',
+        200: '#FCCFCF',
+        300: '#FBB6B6',
+        400: '#FA9E9E',
+        500: '#F88686',
+        DEFAULT: '#F76C6C',
+        700: '#F43E3E',
+        800: '#F20D0D',
+      },
+      blue: {
+        100: '#F7FBFD',
+        200: '#EBF4F9',
+        300: '#D7EAF4',
+        400: '#C4DFEE',
+        500: '#B0D4E8',
+        DEFAULT: '#A8D0E6',
+        700: '#98C7E1',
+        800: '#88BFDD',
+      },
+      'dark-blue': {
+        100: '#A3AFDC',
+        200: '#7E8ECE',
+        300: '#596EBF',
+        400: '#4055A6',
+        500: '#314281',
+        DEFAULT: '#24305E',
+        700: '#151C37',
+        800: '#0E1325',
+      },
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+      backgroundImage: {
+        pattern: "url('/subtle-prism.svg')",
+      },
+    },
   },
   plugins: [],
-}
+};
