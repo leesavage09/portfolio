e50aaf813c8d469b652a19e1002265b43bf6de17
Lee <leesavage09@gmail.com>
2023-03-28 18:10:06 +0300

    feat: changed Button component to internaly use NextJs link

--- src/stories/Atoms/Button.tsx
+++ src/stories/Atoms/Button.tsx
@@ -1,31 +1,30 @@
+import Link from 'next/link';
+import { twMerge } from 'tailwind-merge';
+
 interface ButtonProps {
   primary: boolean;
   label: string;
+  className?: string;
+  href: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({ primary, label }) => {
+export const Button: React.FC<ButtonProps> = ({
+  primary,
+  label,
+  className = '',
+  href,
+}) => {
+  const defaultStyle = `${
+    primary ? 'bg-primary' : 'bg-blue'
+  } text-dark-blue-800 uppercase py-3 px-9 rounded-md drop-shadow-lg border-none duration-300 hover:-translate-y-1`;
+
   return (
-    <button
+    <Link
       type="button"
-      className={`
-            ${primary ? 'bg-primary' : 'bg-blue'} 
-            text-dark-blue-800 
-            
-            uppercase 
-
-            py-3
-            px-9 
-            m-8
-
-            rounded-md 
-            drop-shadow-lg 
-            border-none 
-
-            duration-300 
-            hover:-translate-y-1
-            `}
+      className={twMerge(defaultStyle, className)}
+      href={href}
     >
       {label}
-    </button>
+    </Link>
   );
 };
