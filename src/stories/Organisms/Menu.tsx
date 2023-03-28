f81323d2ccce92024b75032b04f7e1ff936f1b90
Lee <leesavage09@gmail.com>
2023-03-28 16:47:24 +0300

    style: improved menu colours

--- src/stories/Organisms/Menu.tsx
+++ src/stories/Organisms/Menu.tsx
@@ -19,7 +19,7 @@ export const Menu = () => {
     <nav
       className="fixed z-10 top-0 left-0 
     flex justify-between w-full p-4 
-    dark:bg-dark-blue bg-blue-100 drop-shadow-lg 
+    dark:bg-dark-blue-700 bg-blue-100 drop-shadow-lg 
     text-md uppercase font-extrabold     
     "
     >
@@ -28,7 +28,7 @@ export const Menu = () => {
           src={profilePic}
           width={60}
           height={60}
-          className="w-11 h-11 rounded-full dark:bg-dark-blue-700 bg-blue-200"
+          className="w-11 h-11 rounded-full dark:bg-dark-blue-500 bg-blue-200"
           alt="A portrait photo of Lee Savage"
         />
         <a href="#" className="text-primary hover:text-yellow duration-500">
