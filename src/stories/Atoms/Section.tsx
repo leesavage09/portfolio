429e12b23425c19861bd8a299f4803a3b0b0d5fa
Lee <leesavage09@gmail.com>
2023-03-28 16:47:00 +0300

    style: improved section colours

--- src/stories/Atoms/Section.tsx
+++ src/stories/Atoms/Section.tsx
@@ -14,11 +14,19 @@ export const Section: React.FC<SectionProps> = ({
   className,
 }) => {
   const defaultStyle =
-    'dark:bg-dark-blue dark:text-blue-100 bg-blue text-dark-blue-800 flex flex-col justify-center items-center py-32 px-9 w-full';
+    'flex flex-col justify-center items-center py-32 px-9 w-full dark:text-blue-100 text-dark-blue-800 text-lg';
 
   const mergedStyle = twMerge(defaultStyle, className);
 
-  const patternStyle = pattern ? ' bg-pattern bg-cover' : '';
+  const patternStyle = pattern
+    ? ' dark:bg-dark-blue  bg-blue-300 bg-pattern bg-cover'
+    : ' dark:bg-dark-blue-500  bg-blue-200  ';
 
-  return <section className={mergedStyle + patternStyle}>{children}</section>;
+  return (
+    <section className={mergedStyle + patternStyle}>
+      <div className="max-w-screen-lg flex flex-col justify-center items-center">
+        {children}
+      </div>
+    </section>
+  );
 };
