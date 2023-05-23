fd6fab02fa37de4977eec6aae8a62d38dd74471b
Lee <leesavage09@gmail.com>
2023-05-23 17:08:51 +0300

    feat: added tt2 page

---
+++ src/pages/tt2.tsx
@@ -0,0 +1,194 @@
+import { Chip } from '@stories/Atoms/Chip';
+import { Heading } from '@stories/Atoms/Heading';
+import { Layout } from '@stories/Atoms/Layout';
+import { OrderedList } from '@stories/Atoms/OrderedList';
+import { Paragraph } from '@stories/Atoms/Paragraph';
+import { Section } from '@stories/Atoms/Section';
+import { Subtitle } from '@stories/Atoms/Subtitle';
+import Head from 'next/head';
+import Image from 'next/image';
+import tt2Mobile from '/public/images/tt2Mobile.jpg';
+import tt2devices from '/public/images/tt2devices.png';
+
+export default function TT2() {
+  return (
+    <>
+      <Head>
+        <title>TT2 Case Study</title>
+      </Head>
+      <Section id="about" pattern>
+        <Heading type="h1">Tyne Tunnel 2 Project</Heading>
+        <OrderedList
+          items={[
+            {
+              name: 'Client',
+              discription: 'Land digital agency',
+            },
+            {
+              name: 'Role',
+              discription: 'Freelance Front-end Expert',
+            },
+            {
+              name: 'Scope',
+              discription: 'Large monorepo project with multiple front-end',
+            },
+            {
+              name: 'Timeframe',
+              discription: 'Dec 2021 - Apr 2023',
+            },
+          ]}
+        />
+      </Section>
+      <Section id="about">
+        <Layout
+          className="grid grid-cols-2 gap-20"
+          columns={[
+            <>
+              <Heading type="h3">About Tyne Tunnel</Heading>
+              <Paragraph>
+                Tyne Tunnel is the most popular route for those driving to the
+                North of England. The tunnel goes underneath the river Tyne and
+                connects North and South Shields.
+              </Paragraph>
+              <Paragraph>
+                First opened in 1967 as a single lane tunnel in each direction,
+                the tunnel was expanded to a dual carriageway in 2011. Nowadays,
+                the tunnel facilitates over 60,000 journeys each day.
+              </Paragraph>
+              <Paragraph>
+                A cashless free flow system was implemented in November 2021.
+                The removal of barriers and physical payment booths required
+                implementation of a web app payment system.
+              </Paragraph>
+            </>,
+            <>
+              <Heading type="h3">Technologies used</Heading>
+              <div className="flex flex-wrap gap-4 ">
+                <Chip lable={'Agile Scrum'} />
+                <Chip lable={'Gitflow'} />
+
+                <Chip lable={'ClickUp'} />
+                <Chip lable={'Jira'} />
+
+                <Chip lable={'Babel'} />
+                <Chip lable={'Vite'} />
+                <Chip lable={'TypeScript'} />
+                <Chip lable={'React'} />
+
+                <Chip lable={'Eslint'} />
+                <Chip lable={'SASS'} />
+                <Chip lable={'Tailwind'} />
+
+                <Chip lable={'URQL'} />
+                <Chip lable={'GraphQL'} />
+
+                <Chip lable={'Formik'} />
+                <Chip lable={'Yup'} />
+                <Chip lable={'Storybook'} />
+                <Chip lable={'Ant design'} />
+
+                <Chip lable={'Jest'} />
+                <Chip lable={'Cypress'} />
+                <Chip lable={'React Testing Library'} />
+              </div>
+            </>,
+          ]}
+        />
+      </Section>
+
+      <Section id="details">
+        <Heading type="h2">Multiple front-ends</Heading>
+        <Subtitle type="M">
+          I worked as the lead front-end developer on multiple front ends.
+        </Subtitle>
+        <Layout
+          className="grid grid-cols-2 gap-20"
+          columns={[
+            <>
+              <Image
+                src={tt2devices}
+                alt={`TT2 app images`}
+                className="drop-shadow-md"
+                width={500}
+              />
+            </>,
+            <>
+              <Heading type="h3">Customer Web app</Heading>
+              <Paragraph>
+                The customer facing front end is a responsive web app. Customers
+                are able to sign in, view their journeys, pay for tolls manage
+                vehicles and top-up their account.
+              </Paragraph>
+              <Heading type="h3">Head office reporting</Heading>
+              <Paragraph>
+                The front end of head office reporting provides a view on
+                several statistics for tunnel usage, payments, UTN charges and
+                more. It also offers the possibility to export data.
+              </Paragraph>
+              <Heading type="h3">Customer service</Heading>
+              <Paragraph>
+                The back-office for the customer service facilitates speedy,
+                informative, one-touch responses to customers, to enabling team
+                members to exceed internal KPIs.
+              </Paragraph>
+            </>,
+          ]}
+        />
+      </Section>
+
+      <Section id="details">
+        <Heading type="h2">Work Compleated</Heading>
+        <Subtitle type="M">
+          Due to the large scope of the project, continuous updates and ongoing
+          improvements were necessary. Improvements like reduction of technical
+          debt, implementation of features, redesigns, and improvements of user
+          experience.
+        </Subtitle>
+        <Layout
+          className="grid grid-cols-2 gap-20"
+          columns={[
+            <>
+              <Heading type="h3">Stripe implementation</Heading>
+              <Paragraph>
+                When I joined Land Digital agency on the Tyne Tunnel 2 Project,
+                WorldPay was the payment provider. I replaced WorldPay with a
+                customized Stripe check-out.
+              </Paragraph>
+              <Heading type="h3">Authentication</Heading>
+              <Paragraph>
+                I refactored front end authentication logic, simplifying a
+                customized authentication/authorization component. The component
+                handled masquerade tokens allowing telephone support agents
+                access to user accounts.
+              </Paragraph>
+            </>,
+            <>
+              <Heading type="h3">Migration to React router 6</Heading>
+              <Paragraph>
+                To clarify design decisions and help future developers maintain
+                the code base, I refactored and simplified application routing
+                with a migration from React router 5 to React router 6. This
+                included modifications of the use and structure of React 18
+                suspense API and fallback components.
+              </Paragraph>
+              <Heading type="h3">Testing</Heading>
+              <Paragraph>
+                Because of the large number of users, many implemented key
+                features were backed-up with multi-layered (Snapshot, Unit,
+                Integration and E2E) testing for bug-free development.
+              </Paragraph>
+            </>,
+          ]}
+        />
+      </Section>
+      <Section id="images">
+        <Image
+          src={tt2Mobile}
+          alt={`TT2 mobile app images`}
+          className="drop-shadow-md"
+          width={1024}
+        />
+      </Section>
+    </>
+  );
+}
