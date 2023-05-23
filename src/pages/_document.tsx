55f11107a03fa99b275479a21d22a1f9d5844523
Lee <leesavage09@gmail.com>
2023-05-23 17:05:41 +0300

    feat: added head data to _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head />
+      <Head>
+        <meta name="description" content="Lee Savage portfolio Next.js app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
