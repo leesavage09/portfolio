89f0dcc78d8c48d886994122cf8ccdd3e787e823
Lee <leesavage09@gmail.com>
2023-05-24 14:22:07 +0300

    fix: updated Layout to use tailwind compatable styles

--- src/stories/Atoms/Layout.tsx
+++ src/stories/Atoms/Layout.tsx
@@ -1,19 +1,14 @@
 import { twMerge } from 'tailwind-merge';
 
 interface LayoutProps {
-  columns?: number;
+  columns: 1 | 2 | 3;
   elements: Array<{
     className?: string;
     element: JSX.Element;
   }>;
-  className?: string;
 }
 
-export const Layout: React.FC<LayoutProps> = ({
-  elements,
-  className = '',
-  columns = 1,
-}) => {
+export const Layout: React.FC<LayoutProps> = ({ elements, columns }) => {
   const jsx = elements.map((element, idx) => (
     <div
       key={idx}
@@ -26,14 +21,16 @@ export const Layout: React.FC<LayoutProps> = ({
     </div>
   ));
 
-  return (
-    <div
-      className={twMerge(
-        'w-full grid grid-cols-' + columns.toString() + ' gap-x-20 gap-y-24',
-        className
-      )}
-    >
-      {jsx}
-    </div>
-  );
+  const tailwindCol = (() => {
+    switch (columns) {
+      case 1:
+        return 'w-full grid grid-cols-1 gap-x-20 gap-y-24';
+      case 2:
+        return 'w-full grid grid-cols-2 gap-x-20 gap-y-24';
+      case 3:
+        return 'w-full grid grid-cols-3 gap-x-20 gap-y-24';
+    }
+  })();
+
+  return <div className={tailwindCol}>{jsx}</div>;
 };
