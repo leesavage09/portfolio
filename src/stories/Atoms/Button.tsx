b03d70ff73f1dd8eed273a5ec6984f4e0c672149
Lee <leesavage09@gmail.com>
2023-05-26 16:30:33 +0300

    feat: added disable submit button

--- src/stories/Atoms/Button.tsx
+++ src/stories/Atoms/Button.tsx
@@ -8,6 +8,8 @@ interface ButtonProps {
   href?: string;
   scrollTo?: string;
   onClick?: () => void;
+  submit?: boolean;
+  disabled?: boolean;
 }
 
 export const Button: React.FC<ButtonProps> = ({
@@ -17,10 +19,14 @@ export const Button: React.FC<ButtonProps> = ({
   scrollTo,
   href,
   onClick,
+  submit,
+  disabled,
 }) => {
   const defaultStyle = `${
-    primary ? 'bg-primary text-dark-blue-800' : 'bg-blue text-dark-blue'
-  }  uppercase py-3 px-9 rounded-md drop-shadow-lg border-none duration-300 hover:-translate-y-1`;
+    primary
+      ? 'bg-primary text-dark-blue-800 disabled:bg-primary-100 disabled:text-dark-blue-100'
+      : 'bg-blue disabled:bg-blue-100 text-dark-blue disabled:text-dark-blue-100'
+  }  uppercase py-3 px-9 rounded-md drop-shadow-lg border-none duration-300 hover:-translate-y-1 disabled:hover:-translate-y-0`;
 
   const style = twMerge(defaultStyle, className);
 
@@ -46,6 +52,13 @@ export const Button: React.FC<ButtonProps> = ({
   if (onClick)
     return (
       <button type="button" className={style} onClick={onClick}>
+        {label}
+      </button>
+    );
+
+  if (submit)
+    return (
+      <button type="submit" className={style} disabled={disabled}>
         {label}
       </button>
     );
