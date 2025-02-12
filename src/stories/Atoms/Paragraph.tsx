652c59d74053d645e7731f7cefa19ec9ac4a597b
Lee <leesavage09@gmail.com>
2025-02-12 12:40:25 +0200

    feat: updated Paragraph to have tailwind editable classNames

--- src/stories/Atoms/Paragraph.tsx
+++ src/stories/Atoms/Paragraph.tsx
@@ -1,12 +1,22 @@
 import React, { ReactElement } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface ParagraphProps {
   children: ReactElement | string;
+  className?: string;
 }
 
-export const Paragraph: React.FC<ParagraphProps> = ({ children }) => {
+export const Paragraph: React.FC<ParagraphProps> = ({
+  children,
+  className = '',
+}) => {
   return (
-    <p className="mb-10 dark:text-blue-100 text-dark-blue-800 text-base lg:text-lg">
+    <p
+      className={twMerge(
+        'mb-10 dark:text-blue-100 text-dark-blue-800 text-base lg:text-lg',
+        className
+      )}
+    >
       {children}
     </p>
   );
