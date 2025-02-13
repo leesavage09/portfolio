f3adab5e525b3f1a3d6afd0a2b0507eeee3d6ea2
Lee <leesavage09@gmail.com>
2025-02-13 13:49:53 +0200

    feat: added yup and formik to contact form

--- src/stories/Atoms/Input.tsx
+++ src/stories/Atoms/Input.tsx
@@ -1,30 +1,62 @@
-import { HTMLInputTypeAttribute } from 'react';
+import {
+  ChangeEvent,
+  FocusEvent,
+  HTMLInputTypeAttribute,
+  InputHTMLAttributes,
+  TextareaHTMLAttributes,
+} from 'react';
+import { twMerge } from 'tailwind-merge';
 
-interface InputProps {
+interface InputProps
+  extends Pick<
+      InputHTMLAttributes<HTMLInputElement>,
+      'value' | 'placeholder' | 'className'
+    >,
+    Pick<
+      TextareaHTMLAttributes<HTMLTextAreaElement>,
+      'value' | 'placeholder' | 'className'
+    > {
   id: string;
   type?: HTMLInputTypeAttribute;
   label: string;
-  placeholder?: string;
   textArea?: boolean;
+  error?: string;
+  onChange?: (
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ) => void;
+  onBlur?: (
+    e: FocusEvent<HTMLInputElement> | FocusEvent<HTMLTextAreaElement>
+  ) => void;
 }
 
 export const Input: React.FC<InputProps> = ({
   id,
+  className = '',
   type = 'text',
   label,
-  placeholder,
   textArea,
+  error,
+  onChange,
+  onBlur,
+  ...rest
 }) => {
-  const inputStyle =
-    'p-2 rounded-md dark:bg-dark-blue dark:text-blue-100 dark:placeholder:text-blue-800 bg-blue-500 text-dark-blue placeholder:text-dark-blue-300';
+  const inputStyle = twMerge(
+    'p-2 rounded-md dark:bg-dark-blue dark:text-blue-100 dark:placeholder:text-blue-800 bg-blue-500 text-dark-blue placeholder:text-dark-blue-300',
+    className
+  );
 
   return (
     <div className="flex flex-col mb-9">
       <label
-        className="mb-1 dark:text-blue-100 text-dark-blue text-md"
+        className={twMerge(
+          'mb-1  text-md',
+          error
+            ? 'dark:text-primary-100 text-primary font-bold'
+            : 'dark:text-blue-100 text-dark-blue'
+        )}
         htmlFor={id}
       >
-        {label}
+        {error ? error : label}
       </label>
       {!textArea && (
         <input
@@ -32,7 +64,9 @@ export const Input: React.FC<InputProps> = ({
           type={type}
           id={id}
           name={id}
-          placeholder={placeholder}
+          onChange={onChange}
+          onBlur={onBlur}
+          {...rest}
         />
       )}
       {textArea && (
@@ -41,7 +75,9 @@ export const Input: React.FC<InputProps> = ({
           className={`resize-y ${inputStyle}`}
           id={id}
           name={id}
-          placeholder={placeholder}
+          onChange={onChange}
+          onBlur={onBlur}
+          {...rest}
         />
       )}
     </div>
--- src/stories/Molecules/ContactForm.tsx
+++ src/stories/Molecules/ContactForm.tsx
@@ -1,113 +1,132 @@
 import { Input } from '@stories/Atoms/Input';
 import { Modal } from '@stories/Atoms/Modal';
 import { Paragraph } from '@stories/Atoms/Paragraph';
-import { SyntheticEvent, useState } from 'react';
+import { useFormik } from 'formik';
+import { useState } from 'react';
+import * as Yup from 'yup';
 import { Button } from '../Atoms/Button';
 
-type FormEventTarget = typeof EventTarget & {
-  name: { value: string };
-  email: { value: string };
-  message: { value: string };
+interface messageResponse {
+  status?: string;
+  error?: unknown;
+}
+
+const postMessage = async (data: {
+  name: string;
+  email: string;
+  message: string;
+}): Promise<messageResponse> => {
+  const settings = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  try {
+    const fetchResponse = await fetch(`/api/sendMail/`, settings);
+    const data = await fetchResponse.json();
+    return data;
+  } catch (error) {
+    return { error: error };
+  }
 };
 
 export const ContactForm: React.FC = () => {
-  const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const [result, setResult] = useState<{ status: any; error: any }>();
+  const [result, setResult] = useState<messageResponse>();
 
-  const postMessage = async (data: {
-    name: string;
-    email: string;
-    message: string;
-  }) => {
-    const settings = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    };
-    try {
-      const fetchResponse = await fetch(`/api/sendMail/`, settings);
-      const data = await fetchResponse.json();
-      return data;
-    } catch (error) {
-      return { error: error, data: null };
-    }
-  };
-
-  const getFormData = (e: SyntheticEvent) => {
-    const target = e.target as unknown as FormEventTarget;
-    return {
-      name: target.name.value,
-      email: target.email.value,
-      message: target.message.value,
-    };
-  };
-
-  const resetFromData = (e: SyntheticEvent) => {
-    const target = e.target as unknown as FormEventTarget;
-    target.name.value = '';
-    target.email.value = '';
-    target.message.value = '';
-  };
-
-  const submit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    setLoading(true);
-
-    const data = getFormData(e);
-
-    if (
-      data.name ||
-      typeof data.name === 'string' ||
-      data.email ||
-      typeof data.email === 'string' ||
-      data.message ||
-      typeof data.message === 'string'
-    ) {
-      const result = await postMessage(data);
-      setResult(result);
-      if (result?.status === 'OK') resetFromData(e);
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(150, 'The maximum length on name is 200 characters')
+        .required('! Please provide your name'),
+      email: Yup.string()
+        .email('Not a valid email')
+        .required('! Please provide an email'),
+      message: Yup.string()
+        .max(3000, 'The maximum length of your message is 3000 characters')
+        .required('! Please write a message'),
+    }),
+    onSubmit: async (values) => {
+      setResult(undefined);
       setShowModal(true);
-    }
-
-    setLoading(false);
-  };
+      const result = await postMessage(values);
+      setResult(result);
+      if (result?.status === 'OK') formik.resetForm();
+    },
+  });
 
   return (
     <>
       <Modal visible={showModal} close={() => setShowModal(false)}>
-        {result?.status === 'OK' ? (
+        {!result ? (
+          <Paragraph>Your message is sending....</Paragraph>
+        ) : result?.status === 'OK' ? (
           <Paragraph>Message Sent</Paragraph>
         ) : (
           <Paragraph>
-            <>{result?.error}</>
+            Your message could not be sent. Please check you have provided valid
+            data and try again later...
           </Paragraph>
         )}
       </Modal>
       <form
-        onSubmit={submit}
+        onSubmit={formik.handleSubmit}
         className="dark:bg-dark-blue-700 dark:text-blue-100 bg-blue-200 text-dark-blue-800 rounded-md drop-shadow-md p-6 sm:p-8 md:p-12 lg:p-16 w-full"
       >
-        <Input id="name" label="Name" placeholder="Enter your Name" />
-        <Input id="email" label="Email" placeholder="Enter your Email" />
+        <Input
+          id="name"
+          label="Name"
+          placeholder="Enter your Name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.errors.name}
+        />
+        <Input
+          id="email"
+          label="Email"
+          placeholder="Enter your Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.errors.email}
+        />
         <Input
           id="message"
           textArea
           label="Message"
           placeholder="Enter your Message"
+          value={formik.values.message}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.errors.message}
         />
         <Button
           type="submit"
           primary
-          className="float-right"
-          disabled={loading}
+          className="float-right ml-4"
+          disabled={formik.isSubmitting}
           disabledText="Sending Email..."
         >
           Send Email
         </Button>
+        {result &&
+          (result?.status === 'OK' ? (
+            <Paragraph className="text-sm">Message Sent</Paragraph>
+          ) : (
+            <Paragraph className="text-sm">
+              Your message could not be sent. Please check you have provided
+              valid data and try again later...
+            </Paragraph>
+          ))}
       </form>
     </>
   );
