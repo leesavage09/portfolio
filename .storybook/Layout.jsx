d3fae037437d2096019c40dde1850eebf8c7c044
Lee <leesavage09@gmail.com>
2025-02-25 12:20:59 +0200

    refactor: removed storybook due to time consuming security and update issues

--- .storybook/Layout.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { useAppState } from '../src/components/AppContext';
-
-export const Layout = ({ children }) => {
-  const appState = useAppState();
-  return <div className={appState.theme}>{children}</div>;
-};
--- src/stories/Organisms/Menu.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Menu } from './Menu';
-
-export default {
-  title: 'Organisms/Menu',
-  component: Menu,
-} as ComponentMeta<typeof Menu>;
-
-export const menu: ComponentStory<typeof Menu> = () => <Menu />;
