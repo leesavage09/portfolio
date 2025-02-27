8afd0bf2498f2bce7861f35ff82c6a23fe857cfe
Lee <leesavage09@gmail.com>
2025-02-27 13:28:45 +0200

    feat: improved responsive menu

--- src/stories/Organisms/Menu.tsx
+++ src/stories/Organisms/Menu.tsx
@@ -8,7 +8,7 @@ export const Menu = () => (
     className="fixed z-10 top-0 left-0 
     flex justify-between w-full p-4 
     dark:bg-dark-blue-700 bg-blue-100 drop-shadow-lg 
-    text-sm uppercase font-extrabold  
+    text-sm sm:uppercase font-extrabold  
     sm:text-base
     md:text-lg
     "
@@ -23,48 +23,45 @@ export const Menu = () => (
           src={profilePic}
           width={60}
           height={60}
-          className="w-11 h-11 rounded-full dark:bg-dark-blue-500 bg-blue-200 mr-3 hidden md:block"
+          className="w-11 h-11 rounded-full dark:bg-dark-blue-500 bg-blue-200 mr-3 hidden xs:block"
           alt="A portrait photo of Lee Savage"
         />
         <span className="hidden md:block">Lee Savage</span>
       </>
     </Link>
-    <div className="flex items-center ">
-      <ol className="flex flex-wrap space-x-9 justify-end text-blue-100 ">
-        <span className="flex flex-wrap space-x-9 justify-end">
-          <Link
-            href="/"
-            scrollTarget="home"
-            className="text-dark-blue dark:text-blue-100 hover:text-yellow dark:hover:text-yellow duration-500"
-          >
-            Home
-          </Link>
-          <Link
-            href="/"
-            scrollTarget="about"
-            className="text-dark-blue dark:text-blue-100 hover:text-yellow dark:hover:text-yellow duration-500"
-          >
-            About
-          </Link>
-        </span>
-        <span className="flex flex-wrap space-x-9 justify-end">
-          <Link
-            href="/"
-            scrollTarget="projects"
-            className="text-dark-blue dark:text-blue-100 hover:text-yellow dark:hover:text-yellow duration-500"
-          >
-            Projects
-          </Link>
-          <Link
-            href="/"
-            scrollTarget="contact"
-            className="text-dark-blue dark:text-blue-100 hover:text-yellow dark:hover:text-yellow duration-500"
-          >
-            Contact
-          </Link>
-        </span>
+    <div className="flex items-center justify-end w-full xs:w-auto">
+      <ol className="flex flex-wrap justify-between ml-8 xs:justify-end xs:space-x-6 sm:space-x-9 w-full xs:w-auto text-blue-100 ">
+        <Link
+          href="/"
+          scrollTarget="home"
+          className="text-dark-blue dark:text-blue-100 hover:text-yellow dark:hover:text-yellow duration-500"
+        >
+          Home
+        </Link>
+        <Link
+          href="/"
+          scrollTarget="about"
+          className="text-dark-blue dark:text-blue-100 hover:text-yellow dark:hover:text-yellow duration-500"
+        >
+          About
+        </Link>
+
+        <Link
+          href="/"
+          scrollTarget="projects"
+          className="text-dark-blue dark:text-blue-100 hover:text-yellow dark:hover:text-yellow duration-500"
+        >
+          Projects
+        </Link>
+        <Link
+          href="/"
+          scrollTarget="contact"
+          className="text-dark-blue dark:text-blue-100 hover:text-yellow dark:hover:text-yellow duration-500"
+        >
+          Contact
+        </Link>
       </ol>
-      <li className="list-none ml-4">
+      <li className="list-none ml-4 ">
         <ThemeToggle />
       </li>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -95,6 +95,7 @@ module.exports = {
       },
     },
     screens: {
+      xs: '450px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
